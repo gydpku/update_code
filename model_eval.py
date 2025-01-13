192b729ef5f072ab144078c165383f68e7cad820
Shawn Tan <shawn@wtf.sg>
2025-01-13 09:10:54 -0500

    Your descriptive commit message

--- model_eval.py
+++ model_eval.py
@@ -32,7 +32,7 @@ def valid_results_collect(model_path,valid_data,task):
     start_t=time.time()
     if task=='sql':
         failed_cases,correct_cases=sql_evaluation(trained_model,valid_data)
-    elif 'nli' in task.lower() or 'gsm8k' in task.lower():
+    elif 'logiqa' in task.lower() or 'nli' in task.lower() or 'gsm8k' in task.lower():
         failed_cases,correct_cases=batched_evaluation(trained_model,valid_data,task)
     del trained_model
     end_t=time.time()
@@ -53,7 +53,25 @@ def extract_answer_prediction_nli(predicted_output):
         if extract_answer_nli(sen):
             return extract_answer_nli(sen)
     return
+def extract_answer_prediction_logiqa(predicted_output):
+    sens=predicted_output.split('.')
+    final_sens=[sen for sen in sens if 'final' in sen]
+    for sen in final_sens:
+        if extract_answer_logiqa(sen):
+    #        print('extract',sen,'\n',extract_answer_logiqa(sen))
+            return extract_answer_logiqa(sen)
+    return
 def extract_answer_prediction_gsm8k(predicted_output):
+    #print('predicted_output',predicted_output,'\n')
+    if '####' in predicted_output:
+        predicted_output=predicted_output[predicted_output.find('####'):]
+    elif '###' in predicted_output:
+        predicted_output=predicted_output[predicted_output.find('###'):]
+    elif '##' in predicted_output:
+        predicted_output=predicted_output[predicted_output.find('##'):]
+    #else:
+        #predicted_output=predicted_output.split('\n\n')[-2]
+        
     regex_pattern = "(-?[$0-9.,]{2,})|(-?[0-9]+)"
     regexes_to_ignore =[
         ",",
@@ -63,6 +81,9 @@ def extract_answer_prediction_gsm8k(predicted_output):
     ]
     match = re.findall(regex_pattern, predicted_output)
     if match:
+        print(predicted_output,'\n')
+        print(match,'\n')
+        print(match[-1],'\n')
         match = match[-1]
         if isinstance(match, tuple):
             match = [m for m in match if m][0]
@@ -91,10 +112,36 @@ def extract_answer_nli(text):
     if 'entailment' in text.lower():
         return 'entailment'
     return None
+def extract_answer_logiqa(text):
+    matches = re.findall(r'\b[A-D]\b', text)
+    if matches:
+        return matches[0]
+    return None
+
 def extract_answer_gsm8k(text):
-    return float(text.split('####')[-1].strip())
+    try_1=extract_answer_prediction_gsm8k(text)
+    
+    if try_1 is not None:
+        return try_1
+    else:
+        if len(text.split('\n\n'))>1:
+          return extract_answer_prediction_gsm8k(text.split('\n\n')[-2])
+        else:
+          return
+    return try_1 if try_1 is not None else extract_answer_prediction_gsm8k(text.split('\n\n')[-2])
+    #return extract_answer_prediction_gsm8k(text) #float(text.split('####')[-1].strip().replace(',',''))
 def process_batch(data_batch,task,trained_model,failed_cases,correct_cases):
     batch_prompts = [data['Input'] for data in data_batch]
+    if 'gsm8k' in task.lower():
+        #demo_prompt='Here are some demonstration examples:'
+        #demos=torch.load('GSM8k_demo.pt')
+        #for id in range(8):
+        #    demo=demos[id]
+        #    demo_prompt+='\n Example {2}: Input:{0} Output:{1}'.format(demo['Input'],demo['Output'],id+1)
+        batch_prompts = [data['Input']+"\n Let's think step by step. At the end, you MUST write the answer as an number after '####' likes '#### number'." for data in data_batch]
+    if 'nli' in task.lower():
+        aaa=torch.load('MedNLI_demo.pt')
+        batch_prompts = [data['Input']+'Here are some examples:'+'Example 0:'+str(aaa[0])+'\nExample 1:'+str(aaa[1])+'\nExample 2:'+str(aaa[2])+'\nExample 3:'+str(aaa[3])+'\nExample 4:'+str(aaa[4])+"\n End your response with 'The final answer is xxx'." for data in data_batch] 
     outputs = trained_model.generate(batch_prompts, sampling_params)
     if 'nli' in task.lower():
         labels=['entailment','contradiction','neutral']
@@ -106,6 +153,7 @@ def process_batch(data_batch,task,trained_model,failed_cases,correct_cases):
             if not predicted_res:
                 predicted_res=predicted_output
             non_labels = [lbl for lbl in labels if lbl != label]
+         #   print('matched',label,predicted_res,'\n')
             if label not in predicted_res or any(non_label in predicted_res for non_label in non_labels):
                 failed_cases.append((data['Input'],predicted_res,label,data))
             else:
@@ -115,11 +163,27 @@ def process_batch(data_batch,task,trained_model,failed_cases,correct_cases):
             predicted_output = output.outputs[0].text
             predicted_res = extract_answer_prediction_gsm8k(predicted_output)
             label = extract_answer_gsm8k(data['Output'])
+            print(predicted_output,'\n')
+            print('pred extraction and label',predicted_res,label,'\n')
+#            pdb.set_trace()
+#            if label is None:
+            if predicted_res is None or label is None:
+                failed_cases.append((data['Input'],predicted_res,label,data))
+            
+            elif abs(predicted_res-label)>1e-3:
+                failed_cases.append((data['Input'],predicted_res,label,data))
+            else:
+                correct_cases.append((data['Input'],predicted_res,label,data))
+    elif 'logiqa' in task.lower():
+        for data, output in zip(data_batch, outputs):
+            predicted_output = output.outputs[0].text
+            predicted_res = extract_answer_prediction_logiqa(predicted_output)
+            label = extract_answer_logiqa(data['Output'])
             print(predicted_res,label,'\n')
             print('pred and result',predicted_res,label,'\n')
             if predicted_res is None:
                 failed_cases.append((data['Input'],predicted_res,label,data))
-            elif abs(predicted_res-label)>1e-3:
+            elif predicted_res != label:
                 failed_cases.append((data['Input'],predicted_res,label,data))
             else:
                 correct_cases.append((data['Input'],predicted_res,label,data))
@@ -207,6 +271,8 @@ def load_and_generate_predictions(model_path, batch_prompts,seed=True, seeds=['2
 def safty_float_check(pred,label):
     if pred is None:
         return True
+    if label is None:
+        return False
     if abs(float(pred)-float(label))>1e-3:
         return True
     return False
@@ -216,9 +282,11 @@ def process_single_example(data, outputs, id, task):
         label = extract_answer_nli(data['Output'].split('is')[-1])
     elif 'gsm8k' in task.lower():
         label = extract_answer_gsm8k(data['Output'])
+    elif 'logiqa' in task.lower():
+        label = extract_answer_logiqa(data['Output']) #data['Output']
     predictions = []
     predicted_outputs = []
-    
+    #print('labels',data['Output'],'\n',extract_answer_prediction_logiqa(data['Output']),'\n',extract_answer_logiqa(data['Output']))
     # Collect predictions
     for output in outputs:
 #        pdb.set_trace()
@@ -229,8 +297,11 @@ def process_single_example(data, outputs, id, task):
                 predicted_res = extract_answer_nli(predicted_output)
         elif 'gsm8k' in task.lower():
             predicted_res = extract_answer_prediction_gsm8k(predicted_output)
+        elif 'logiqa' in task.lower():
+            predicted_res = extract_answer_logiqa(predicted_output)
         predictions.append(predicted_res)
         predicted_outputs.append(predicted_output)
+    #print('preds',predictions,'\n','answer',label,'\n')
     # Get incorrect predictions
     if 'nli' in task.lower():
         incorrect_outputs = [
@@ -239,6 +310,19 @@ def process_single_example(data, outputs, id, task):
                 if pred != label
             ]
         i_o=[]
+        for output in incorrect_outputs:
+            if "Let's think step by step." in output:
+                i_o.append(output.split("Let's think step by step.")[1])
+            else:
+                i_o.append(output)
+    elif 'logiqa' in task.lower():
+        incorrect_outputs= [
+            output
+            for output, pred in zip(predicted_outputs, predictions)
+                if pred != label
+        ]
+        i_o=[]
+
         for output in incorrect_outputs:
             if "Let's think step by step." in output:
                 i_o.append(output.split("Let's think step by step.")[1])
@@ -266,6 +350,8 @@ def process_single_example(data, outputs, id, task):
 def process_validation_batch_major(model_path, data_batch,task,seed=True,  iteration=5,temperature=0.2):
     """Main function to process NLI validation batch"""
     batch_prompts = [data['Input'] for data in data_batch]
+    if 'gsm8k' in task.lower():
+        batch_prompts = [data['Input']+"\n Let's think step by step. At the end, you MUST write the answer as an number after '####' likes '#### number'." for data in data_batch]
     outputs = load_and_generate_predictions(model_path, batch_prompts,seed=seed, iteration=iteration,temperature=temperature)
     ooa_failed_cases = []  # out-of-agreement failures
     im_failed_cases = []   # inconsistent-majority failures
@@ -288,11 +374,23 @@ def process_validation_batch_major(model_path, data_batch,task,seed=True,  itera
                 # All predictions correct
                 correct_cases.append((data, predicted_outputs))
         elif 'gsm8k' in task.lower():
-            if safty_float_check(majority_prediction,label):
-   
+#            print(majority_prediction,label,'\n')
+ #           pdb.set_trace()
+            if majority_prediction is not None:
+                if not safty_float_check(majority_prediction,label):
+                    correct_cases.append((data, predicted_outputs))
+                else:
+                    ooa_failed_cases.append((data, incorrect_outputs))
+            else:
+                ooa_failed_cases.append((data, incorrect_outputs))
+        elif 'logiqa' in task.lower():
+            if majority_prediction != label:
+                # Out of agreement failure - majority prediction is wrong
+    #           pdb.set_trace()
                 ooa_failed_cases.append((data, incorrect_outputs))
 
             else:
+                # All predictions correct
                 correct_cases.append((data, predicted_outputs))
 
     return ooa_failed_cases, im_failed_cases, correct_cases
@@ -313,6 +411,13 @@ def process_validation_batch_count(model_path, data_batch,task,seed=True,  itera
 #        pdb.set_trace()
         # Categorize results
         if 'nli' in task.lower():
+            count=0
+            for pred in predictions:
+                if pred is not None:
+                    if pred==label:
+                        count+=1
+            data_count.append((data,count/len(predictions)))
+        elif 'logiqa' in task.lower():
             count=0
             for pred in predictions:
                 if pred is not None:
